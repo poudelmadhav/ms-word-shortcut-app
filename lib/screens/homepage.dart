b1496ed2354640d79fb60eb83b4d2addc7a828cb
Madhav Paudel <poudelmadhav143@gmail.com>
2020-04-26 16:17:55 +0545

    app drawer added in the app inside Scaffold with Drawer DrawerHeader

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -6,21 +6,60 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              padding: EdgeInsets.all(0),
+              child: Container(
+                color: Colors.tealAccent,
+                child: Center(
+                  child: Text(
+                    "MS Word Guide",
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 30.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.info_outline),
+              title: Text("About"),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return AboutPage();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
-        leading: Icon(Icons.menu),
         title: Text("MS Word Shortcuts"),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.info_outline),
             iconSize: 26,
             onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return AboutPage();
-                    },
-                  ));
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return AboutPage();
+                },
+              ));
             },
           ),
         ],
