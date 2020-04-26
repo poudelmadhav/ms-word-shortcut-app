a9ca303abb20e1b80d7b6d81d22efc2d3509e704
Madhav Paudel <poudelmadhav143@gmail.com>
2020-04-26 16:03:31 +0545

    content in about page added and custom go back functionality too

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -7,6 +7,31 @@ class AboutPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("About app"),
       ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "This is the app where you can find the shortcut keys using with Microsoft Word.",
+                style: TextStyle(
+                  fontSize: 17.0,
+                ),
+              ),
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            color: Colors.tealAccent,
+            child: Text(
+              "Go Back",
+              style: TextStyle(color: Colors.black),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
